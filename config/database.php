bb6bd92135aae52d6fa570f5f43c4769c727b624
wissamlakhdari <wissam.lakhdari@ensia.edu.dz>
2025-01-04 09:25:36 +0100

    delete unecessary files

--- config/database.php
+++
@@ -1,5 +0,0 @@
-<?php
-return new PDO('mysql:host=localhost;dbname=expense_tracker', 'root', '', [
-    PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
-    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-]);
--- models/BaseModel.php
+++
@@ -1,9 +0,0 @@
-<?php
-class Model {
-    protected $db;
-
-    public function __construct() {
-        $this->db = require __DIR__ . '/../config/database.php';
-    }
-}
-?>
